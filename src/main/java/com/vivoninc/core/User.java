6e26cb0291be59d01477d85c9ec370a358df96fe
Josef Sundin <js225uz@student.lnu.se>
2025-05-29 19:39:35 +0200

    Initialized spring boot project

---
+++ src/main/java/com/vivoninc/core/User.java
@@ -0,0 +1,17 @@
+package com.vivoninc.core;
+
+public class User {
+    String userName;
+
+    public User(String userName){
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
