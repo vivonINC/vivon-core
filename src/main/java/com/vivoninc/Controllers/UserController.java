9649b3ba0000562202e5939ed964454b7f4f7a17
Josef Sundin <js225uz@student.lnu.se>
2025-06-23 09:52:36 +0200

    feat: friend related controller methods added

--- src/main/java/com/vivoninc/Controllers/UserController.java
+++ src/main/java/com/vivoninc/Controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,22 @@ public class UserController {
 
     @GetMapping("/friends") //Should work
         public Collection<User> getUsersFriends(int id){
-            return userDAO.getUsersFriends(0);
+            return userDAO.getUsersFriends(id);
         }
+    
+    @GetMapping("/incFriendRequests")
+    public Collection<User> getFriendRequests(int id){
+        return userDAO.getUsersIncommingFriendReq(id);
+    }
+
+    @PostMapping("/sendFriendRequest")
+    public void sendFriendRequest(int myID, int friendID){
+        userDAO.sendFriendRequest(myID, friendID);
+    }
+
+    @PostMapping("/acceptFriendRequest")
+    public void acceptFriendRequest(int myID, int friendID){
+        userDAO.acceptFriendRequest(myID, friendID);
+    }
 
 }
